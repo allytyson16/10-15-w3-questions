ba59abb3336697d744ae438d253e8934612aa83e
Adelaide <88384474+allytyson16@users.noreply.github.com>
2022-02-05 18:46:36 +0000

    Update question15.cs

--- question15.cs
+++ question15.cs
@@ -9,8 +9,8 @@ namespace w3_schools
 
     //15. Write a C# program remove specified a character from a non-empty string using index of a character
 
-		    	Console.WriteLine("Hello, Universe, My name is Adelaide and my journey in C# is to arrive as a Game Developer.😁");
-		    	string specified = "Hello, Universe, My name is Adelaide and my journey in C# is to arrive as a Game Developer.😁";
+		    	Console.WriteLine("Hello, Universe, My name is Adelaide and my journey in C# is to arrive as a Game Developer.");
+		    	string specified = "Hello, Universe, My name is Adelaide and my journey in C# is to arrive as a Game Developer.";
 			//deleting 17 characters.
    			  String charRemoved = specified.Remove(0, 17);
 			    Console.WriteLine(charRemoved);
